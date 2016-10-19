a834d832076517f9c3faeb6def3214648d5211ca
joehee <r>
2016-10-19 10:09:49 +0900

    앱메뉴 구성 수정
    - 현재 버전 표기
    - 메뉴 타이틀 문구 수정

--- websrc/www/js/menuapp.js
+++ websrc/www/js/menuapp.js
@@ -3,6 +3,7 @@ function loadAppMenu(container){
     var datasource={};
     $(container).load('appmenu.html', function() {
         $(this).trigger('create');
+        $('#menu_version').text('현재 버전:'+ BuildInfo.version);
     });
 }
 
